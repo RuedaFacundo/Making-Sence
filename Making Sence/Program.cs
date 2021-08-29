2f619dd3bf47cc954dbffc06ad034ae0034399f8
Facundo <facundoarueda@hotmail.com>
2021-08-29 14:54:00 -0300

    Agregar archivos de proyecto.

---
+++ Making Sence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Making_Sence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
