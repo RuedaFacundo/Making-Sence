49d41c6742c2ca478ec4fbc0cdd66d0213a5490e
Facundo <facundoarueda@hotmail.com>
2021-09-05 13:01:09 -0300

    Create Update and Delete methods

--- Making Sense/Models/Program.cs
+++ Making Sense/Models/Program.cs
@@ -25,9 +25,33 @@ namespace Making_Sense
                 Doors = 3,
                 Type = Type.MANUAL
             });
+            Car Megane = Cars.Create(new Car
+            {
+                CarID = 2,
+                Brand = Brand.RENAULT,
+                Model = "Megane",
+                Color = "Black",
+                Doors = 5,
+                Type = Type.AUTOMATIC
+            });
             Console.WriteLine(Palio.ToString());
             Console.WriteLine(Fox.ToString());
+            Console.WriteLine(Megane.ToString());
             Console.WriteLine(Cars.Get(0).ToString());
+            Console.WriteLine(Cars.Get(2).ToString());
+            Car Corsa = new Car
+            {
+                CarID = 1,
+                Brand = Brand.CHEVROLET,
+                Model = "Corsa",
+                Color = "Blue",
+                Doors = 5,
+                Type = Type.MANUAL
+            };
+            Console.WriteLine("El auto actualizado: ");
+            Console.WriteLine(Cars.Update(Corsa).ToString());
+            Cars.Delete(1);
+
         }
     }
 }
