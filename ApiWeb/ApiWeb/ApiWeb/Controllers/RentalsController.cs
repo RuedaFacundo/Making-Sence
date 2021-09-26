9eb835dbbaa09e259e5a08f5e7073d4c78622894
Facundo <facundoarueda@hotmail.com>
2021-09-26 13:52:17 -0300

    Feat[Controllers]: Add controllers of cars, customers and rentals

---
+++ ApiWeb/ApiWeb/ApiWeb/Controllers/RentalsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiWeb.Models;
+
+namespace ApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public RentalsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rentals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rental>>> GetRentals()
+        {
+            return await _context.Rentals.ToListAsync();
+        }
+
+        // GET: api/Rentals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rental>> GetRental(long id)
+        {
+            var rental = await _context.Rentals.FindAsync(id);
+
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return rental;
+        }
+
+        // PUT: api/Rentals/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRental(long id, Rental rental)
+        {
+            if (id != rental.RentalId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rental).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RentalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rentals
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Rental>> PostRental(Rental rental)
+        {
+            _context.Rentals.Add(rental);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRental", new { id = rental.RentalId }, rental);
+        }
+
+        // DELETE: api/Rentals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRental(long id)
+        {
+            var rental = await _context.Rentals.FindAsync(id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rentals.Remove(rental);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RentalExists(long id)
+        {
+            return _context.Rentals.Any(e => e.RentalId == id);
+        }
+    }
+}
--- ApiWeb/ApiWeb/ApiWeb/Models/Rental.cs
+++ ApiWeb/ApiWeb/ApiWeb/Models/Rental.cs
@@ -4,6 +4,7 @@ namespace ApiWeb.Models
 {
     public class Rental
     {
+        public long RentalId { get; set; }
         public string Duration { get; set; }
         public Car Car { get; set; }
         public Customer Client { get; set; }
